2821e978d9ddd2af657fa3bf2c92094666b6d010
mhpob <mikeob9@gmail.com>
2014-07-23 17:34:35 -0400

    Removed dependence on reshape package and cleaned up code. Now uses base functions and lubridate.

--- wunderimp.R
+++ wunderimp.R
@@ -1,38 +1,44 @@
-############################################################
+###############################################################################
 ## Import archived meteorological data from wunderground.com
-## This function uses reshape package to extract data frames
-##   from list (k, here) and merge them into a single data
-##   frame.
-##   Please note, merge_recurse() is not in reshape2 package.
 ##
-## arp = airport code (in quotes)
-## Yr = sequence of years desired
-## Mst = start month
-## Mend = end month
+## station = station name or airport code (in quotes)
+## start = start date (ymd, in quotes)
+## end = end date (ymd, in quotes)
 ##
-## Example: data <- wunderimp('KWAL',seq(1977,1989,1),1,12)
-############################################################
+## Example: data <- wunderimp('KWAL', '1999-01-10', '2005-10-15')
+###############################################################################
 ## Frequently used airport codes:
-## OC, MD == KOXB
+## Ocean City, MD == KOXB
 ## Wallops Island, MD  == KWAL
 ## PAX NAS == KNHK
-############################################################
+###############################################################################
 
 if(('lubridate' %in% installed.packages()) == F) install.packages('lubridate')
+library(lubridate)
 
-wunderimp <- function (arp, Yr, Mst, Mend) {
-  Dend <- ifelse(Mend == 4 | Mend == 6 | Mend == 9 | Mend == 11, 30, 
-                 ifelse(Mend == 2, 28, 31))
-  j <- paste("http://www.wunderground.com/history/airport/",arp,"/",Yr,"/",Mst,
-        "/1/CustomHistory.html?dayend=",Dend,"&monthend=",Mend,"&yearend=",Yr,
-        "&format=1",sep="")
-  k <- lapply(j, read.csv)
-  k <- merge_recurse(k)
- 
-  winddeg<-data.frame(do.call('rbind',strsplit(as.character(k$WindDirDegrees.br...),'<',fixed=T)))
-  k<-cbind(k,winddeg)
-  k<-k[,!names(k) %in% c('WindDirDegrees.br...','X2')]
-  names(k)[1] <- "Date"
-  names(k)[23]<- "WindDirDeg"
-  k
+wunderimp <- function (station, start, end = Sys.Date()) {
+  end <- ymd(end)
+  startyr <- year(seq(ymd(start), end, by = 'year'))
+  
+  url <- paste0("http://www.wunderground.com/history/airport/", station, "/",
+                startyr, "/", month(start), "/", day(start),
+                "/CustomHistory.html?dayend=", day(end), "&monthend=", month(end),
+                "&yearend=", year(end), "&format=1")
+  
+  data <- lapply(url, read.csv)
+  
+  # rbind() will not work unless all colnames are the same. If start and end
+  # time zones are different, this is reflected in Wundergrounds colnames.
+  for(i in seq(1:length(data))){
+    names(data[[i]])[1] <- 'Date'
+  }
+
+  data <- do.call(rbind.data.frame, data)
+  date <- unique(data)
+  
+  # Remove HTML breaks from final column
+  data[,23] <- as.numeric(do.call(rbind, strsplit(as.character(data[, 23]), '<br />')))
+  names(data)[23]<- "WindDirDeg"
+  
+  data
 }
