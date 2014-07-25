f5e333f680125449343792d44deb4c6e8755920e
mhpob <mikeob9@gmail.com>
2014-07-25 17:00:31 -0400

    Automatic formatting and XML hints.

--- PierRip.R
+++ PierRip.R
@@ -2,7 +2,7 @@
 # https://stormcentral.waterlog.com/SiteDetails.php?a=88&site=1&pa=CBLPier
 # 
 # In the future, I'd like to put in start/stop time arguments.
-
+library(lubridate)
 
 xmltree <- readLines('https://stormcentral.waterlog.com/xml/SiteDetailsSiteData.php?Site=1&Acct=88&Start=0&Range=864000000')
 
@@ -11,14 +11,25 @@ datacode <- unlist(strsplit(datacode, '</SiteData>'))[1]
 
 pierdata <- read.csv(paste0('https://stormcentral.waterlog.com/download.php/data/',
                       datacode, '-d.csv?filename=thisdoesntmatter'),
-                      skip = 1, header = T, stringsAsFactors = F)
+                      skip = 1, header = T, 
+                      na.strings = c('NA', '-99.99'), stringsAsFactors = F)
 
+# Drop short-lived second sonde
+pierdata <- pierdata[,-(3:8)]
 
+# Drop empty samples
+pierdata <- pierdata[rowSums(is.na(pierdata)) != 8,]
 
-# j <- getURL('https://stormcentral.waterlog.com/xml/SiteDetailsSiteData.php?Site=1&Acct=88&Start=0&Range=864000000', ssl.verifypeer = F)
-# jj<-xmlTreeParse(j, getDTD = F)
-# jjk <- xmlRoot(jj)
-# jjk[[1]]['SiteData']
+# Formatting
+pierdata$Date <- mdy_hms(paste(pierdata$Date, pierdata$Time))
+pierdata <- pierdata[, -2]
+
+# Using RCurl, XML packages
+# xmltree <- getURL('https://stormcentral.waterlog.com/xml/SiteDetailsSiteData.php?Site=1&Acct=88&Start=0&Range=864000000', ssl.verifypeer = F)
+# root <- xmlRoot(xmlTreeParse(xmltree, getDTD = F))
+# datacode <- xmlSApply(root[[1]], xmlValue)[1]
+#
+# Then continue with 'pierdata <- read.csv(...)'
 
 
 
