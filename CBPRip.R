21601ee7b907abbe7de572cad51e4d760af11def
mhpob <mikeob9@gmail.com>
2014-07-30 13:28:02 -0400

    Figuring out XML.

--- CBPRip.R
+++ CBPRip.R
@@ -2,8 +2,13 @@ CBP Water Quality Database(1984-present) CBP_WQDB
 CBP Water Quality Database(1949-1982) CBI_WQDB 
 http://api.chesapeakebay.net/rest/DataHubRESTSrv/dhHelper.svc/getExtentData/WATER_BODIES/Water_Quality_Data/CBP_WQDB 
 
-j<-xmlTreeParse('http://api.chesapeakebay.net/getWQWaterQuality.svc/WATER_BODIES/101,110,29,40,66,81,93/51/false/12311983/05212010')
+j<-readLines('http://api.chesapeakebay.net/getWQWaterQuality.svc/WATER_BODIES/101,110,29,40,66,81,93/51/false/12311983/05212010')
 jj<-xmlRoot(j)
+p <- xmlSApply(jj, function(x) xmlSApply(x, xmlValue))
+pt <- data.frame(t(p), row.names = NULL)
+
+ptt <- apply(pt,2,unlist)
+
 # Sassafras 101
 #Susquehanna 110
 # Choptank 29
@@ -11,3 +16,6 @@ jj<-xmlRoot(j)
 # Nanticoke 66
 # Pax 81
 # Potomac 93
+
+API Loc:
+http://data.chesapeakebay.net/doc/Datahub_2011_REST_API.pdf
