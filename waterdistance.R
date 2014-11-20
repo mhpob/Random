db90cf051487aa9afbe299249ef300f3cdb3bcc7
mhpob <mikeob9@gmail.com>
2014-11-20 11:05:27 -0500

    Clean things up, accomodate all sites.

--- waterdistance.R
+++ waterdistance.R
@@ -1,27 +1,27 @@
-library(marmap); library(gdistance); library(raster); library(dplyr)
-library(rgdal)
-# From marmap package, but bathymetric data doesn't go as far inland as needed.
-# hudson <- getNOAA.bathy(-75.6, -73.6, 38.7, 42.7, 1)
+library(dplyr); library(gdistance); library(raster); library(rgdal)
 
-# Will now try to use custom shapefile
+# Use custom shapefile
 midstates <- shapefile('p:/obrien/gis/shapefiles/midatlantic/matl_states_land.shp')
 
 # Create nonsense raster file to clip shapefile
-ras.back <- raster(extent(-75.6, -73.6, 38.7, 42), ##CHANGE TO ACCOMODATE ALL SITES!!!!!!!
-                   resolution = 1/720, #Half arc-second grids
+ras.back <- raster(extent(-76.4, -69.89, 36.9, 42.7),
+                   resolution = 1/360, #5 arc-second grids = 720, 10 = 360
                    vals = 1,
                    crs = proj4string(midstates))
-# Also need to clip out a large portion of what will be ocean for memory
-mem.crop <- cbind(c(-75.8, -69.8, -69.8, -75.8), c(37, 37, 41.2, 37))
+
+# Also need to clip out a large portion of what will be ocean to save memory
+mem.crop <- cbind(c(-75.8, -69.89, -69.89, -75.8), c(36.9, 36.9, 41.2, 36.9))
 mem.crop <- SpatialPolygons(list(Polygons(list(Polygon(mem.crop)),
                                           'Memory-Wasting Ocean')),
                             proj4string = CRS(proj4string(midstates)))
 
-ras.tri <- mask(ras.back, mem.crop, inverse = T)
-
 # Create clipped raster from the shapefile-- Rasterize the water, drop the land
-ras.water <- mask(ras.back, midstates, inverse = T)
-# Transition matrix
+ras.water <- mask(mask(ras.back, mem.crop, inverse = T),
+                  midstates, inverse = T)
+
+rm(midstates, ras.back, mem.crop)
+
+# Transition matrix between raster cells
 hud.trans16 <- transition(ras.water, transitionFunction = function(x){1}, 16)
 # Geographic correction
 hud.geo16 <- geoCorrection(hud.trans16, type = 'c')
@@ -45,25 +45,26 @@ all.mean <- allsites %>%
 row.names(all.mean) <- all.mean[, 1]
 all.mean <- all.mean[, c(3, 2)]
 
+rm(hud.trans16, uecsites, lecsites, allsites)
 
 # Calculate paths using lc.dist() from marmap (iterative calc of multiple paths)
-paths <- lc.dist(hud.geo16, all.mean, res = "path")
+paths <- marmap::lc.dist(hud.geo16, all.mean, res = "path")
 # Calculate distances of paths. Note that distances are in rounded km.
-distances <- lc.dist(hud.geo16, all.mean, res = 'dist')
+distances <- marmap::lc.dist(hud.geo16, all.mean, res = 'dist')
 
-# Check that the paths worked
+# Quick check that the paths worked
 plot(ras.water, col = 'grey')
 lines(paths[[1]][,1], paths[[1]][,2], col = 'red', lwd = 2)
-points(uec.mean, col = 'blue')
+points(all.mean, col = 'blue')
 
 # Create GEarth KML paths
-k <- lapply(paths, cbind, 0)
+paths <- lapply(paths, cbind, 0)
 
 for(i in seq(1,length(k))){ 
 cat('<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://earth.google.com/kml/2.1">\n<Document>\n<Placemark>\n<name>Path A</name>\n<LineString>\n<tessellate>1</tessellate>\n<coordinates>\n',
-    file = paste0('output',i,'.kml'))
+    file = paste0('path',i,'.kml'))
 write.table(k[[i]], row.names = F, col.names = F, sep = ',',
-            file = paste0('output',i,'.kml'), append = T)
+            file = paste0('path',i,'.kml'), append = T)
 cat('</coordinates>\n</LineString>\n</Placemark>\n</Document>\n</kml>',
-    file = paste0('output',i,'.kml'), append = T)
+    file = paste0('path',i,'.kml'), append = T)
 }
