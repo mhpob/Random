599fee63807a58e264ef6975ec16cd595f946021
mhpob <mikeob9@gmail.com>
2014-11-24 13:44:26 -0500

    Remove marmap dependency.

--- waterdistance.R
+++ waterdistance.R
@@ -1,18 +1,24 @@
-library(dplyr); library(gdistance); library(raster); library(rgdal)
-# library(gmailr) # used to email me after a long run
-# gmail_auth('auth.json', 'full')
+library(gdistance); library(raster); library(rgdal); library(dplyr)
+library(gmailr) # used to email me after a long run
+gmail_auth('p:/obrien/randomr/auth.json', 'compose')
+options(error = function(){
+  library(gmailr)
+  send_message(mime() %>% to('2679701973@txt.att.net') %>%
+                 subject('Crap.')) # Send message when R throws an error
+})
+# options(error = NULL) # Reset error options (no message)
 
 # Use custom shapefile
 midstates <- shapefile('p:/obrien/gis/shapefiles/midatlantic/matl_states_land.shp')
 
 # Create nonsense raster file to clip shapefile
-ras.back <- raster(extent(-76.4, -69.89, 36.9, 42.7),
+ras.back <- raster(extent(-76.4, -69.8, 36.85, 42.7),
                    resolution = 1/360, #5 arc-second grids = 720, 10 = 360
                    vals = 1,
                    crs = proj4string(midstates))
 
 # Also need to clip out a large portion of what will be ocean to save memory
-mem.crop <- cbind(c(-75.8, -69.89, -69.89, -75.8), c(36.9, 36.9, 41.2, 36.9))
+mem.crop <- cbind(c(-75.8, -69.8, -69.8, -75.8), c(36.85, 36.85, 41.2, 36.85))
 mem.crop <- SpatialPolygons(list(Polygons(list(Polygon(mem.crop)),
                                           'Memory-Wasting Ocean')),
                             proj4string = CRS(proj4string(midstates)))
@@ -34,10 +40,10 @@ hud.geo16 <- geoCorrection(hud.trans16, type = 'c')
 
 #Import site locations
 uecsites <- read.csv(
-  'c:/users/secor lab/desktop/hbailey/nyharbour_datafile_uec.csv',
+  'p:/hbailey/nyharbour_datafile_uec.csv',
   header = T, stringsAsFactors = F)
 lecsites <- read.csv(
-  'c:/users/secor lab/desktop/hbailey/nyharbour_datafile_lec.csv',
+  'p:/hbailey/nyharbour_datafile_lec.csv',
   header = T, stringsAsFactors = F)
 allsites <- rbind(uecsites, lecsites)
 
@@ -53,10 +59,34 @@ all.mean <- all.mean[, c(3, 2)]
 
 rm(hud.trans16, uecsites, lecsites, allsites)
 
-# Calculate paths using lc.dist() from marmap (iterative calc of multiple paths)
-paths <- marmap::lc.dist(hud.geo16, all.mean, res = "path")
+# Calculate paths
+lc.dist <- function (trans, loc, res = c("dist", "path")){
+  # Code directly stolen then slightly edited from marmap package
+  if (res == "dist") {
+    cost <- costDistance(trans, as.matrix(loc))/1000
+    return(round(cost, digits = 2))
+  }
+    if (res == "path") {
+        nb.loc <- nrow(loc)
+        path <- list()
+        comb <- combn(1:nb.loc, 2)
+        pb <- txtProgressBar(min = 0, max = ncol(comb), style = 3)
+        for (i in 1:ncol(comb)) {
+            origin <- sp::SpatialPoints(loc[comb[1, i], ])
+            goal <- sp::SpatialPoints(loc[comb[2, i], ])
+            temp <- gdistance::shortestPath(trans, origin, goal, 
+                output = "SpatialLines")
+            path[[i]] <- temp@lines[[1]]@Lines[[1]]@coords
+            setTxtProgressBar(pb, i)
+        }
+        close(pb)
+        return(path)
+    }
+}
+
+paths <- lc.dist(hud.geo16, all.mean, res = "path")
 # Calculate distances of paths. Note that distances are in rounded km.
-distances <- marmap::lc.dist(hud.geo16, all.mean, res = 'dist')
+distances <- lc.dist(hud.geo16, all.mean, res = 'dist')
 # send_message(mime() %>% to('2679701973@txt.att.net') %>%
 #                subject('Paths done.'))
 
