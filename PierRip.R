209a52c134b19898a0561423b4029cec13b92e28
mhpob <mikeob9@gmail.com>
2015-04-03 11:05:09 -0400

    Update for new sonde.

--- PierRip.R
+++ PierRip.R
@@ -1,5 +1,5 @@
 # This function rips water quality data from the CBL Pier Storm Central Water Log site
-# https://stormcentral.waterlog.com/SiteDetails.php?a=88&site=1&pa=CBLPier
+# http://stormcentral.waterlog.com/SiteDetails.php?a=88&site=2&pa=CBLPier
 # between specified dates/times in ymd_hms format
 
 cblpier <- function(stdate = '2014-03-04 00:00:00',
@@ -17,15 +17,15 @@ cblpier <- function(stdate = '2014-03-04 00:00:00',
   
   range <- enddate - stdate
   
-  url <- paste0('https://stormcentral.waterlog.com/xml/SiteDetailsSiteData.php?',
-                'Site=1&Acct=88&Start=', stdate, '&Range=', range)
+  url <- paste0('http://stormcentral.waterlog.com/xml/SiteDetailsSiteData.php?',
+                'Site=2&Acct=88&Start=', stdate, '&Range=', range)
   
   xmltree <- readLines(url)
   
   datacode <- unlist(strsplit(xmltree[3], '<SiteData>'))[2]
   datacode <- unlist(strsplit(datacode, '</SiteData>'))[1]
   pierdata <- read.csv(
-    paste0('https://stormcentral.waterlog.com/download.php/data/',
+    paste0('http://stormcentral.waterlog.com/download.php/data/',
            datacode, '-d.csv?filename=thisdoesntmatter'),
     skip = 1, header = T,
     na.strings = c('NA', '-99.99'), stringsAsFactors = F)
@@ -36,20 +36,13 @@ cblpier <- function(stdate = '2014-03-04 00:00:00',
   }
   
   # Drop empty samples
-  pierdata <- pierdata[rowSums(is.na(pierdata)) != 8,]
+  pierdata <- pierdata[rowSums(is.na(pierdata)) != 13,]
   
   # Formatting
   pierdata$Date <- mdy_hms(paste(pierdata$Date, pierdata$Time))
   pierdata <- pierdata[, -2]
   row.names(pierdata) <- NULL
-  names(pierdata)[2:9] <- c('Temp', 'SpCond', 'Turb', 'DO_pct',
-                            'DO_con', 'Depth', 'Sal', 'Batt')
-  units <- list(Temp = 'Degrees Celsius', SpCond = 'microSeimens/centimeter',
-           Turb = 'Nephelometric Turbidity Units', DO_pct = 'Percent Saturation',
-           DO_con = 'milligrams/Liter', Depth = 'Meters', Sal = '',
-           Batt = 'Volts')
-  
-  list(units, pierdata)
+  names(pierdata)[2:12] <- lapply(strsplit(names(pierdata)[2:12], '[.]'), '[', 2)
 }
 
 
