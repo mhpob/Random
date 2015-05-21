5dd329d6868f1dfa0dbc028d2611eb345c1a09b7
mhpob <mikeob9@gmail.com>
2015-05-21 19:54:17 -0400

    Working frame_split function. Closes #1 and closes #3.

--- frame_split.R
+++ frame_split.R
@@ -1,12 +1,29 @@
 # Install FFmpeg on your computer http://www.wikihow.com/Install-FFmpeg-on-Windows
-# ffmpeg code
-# Location of ffmpeg on computer
-ff.loc <- 'c:/ffmpeg/bin/ffmpeg.exe'
 
-# Call system command prompt. If there's a warning message, something is wrong.
-system2(ff.loc, '-i p:/obrien/aris/2014-07-16_214123_1_495.mp4 -f image2 aris-%03d.jpeg -v warning')
+frame_split <- function(video, ff.loc, prefix = 'img'){
+  if(substr(ff.loc, nchar(ff.loc), nchar(ff.loc)) == '/'){
+    probe <- paste(ff.loc, 'ffprobe.exe', sep = '')
+    mpeg <-  paste(ff.loc, 'ffmpeg.exe', sep = '')
+  } else{
+    probe <- paste(ff.loc, 'ffprobe.exe', sep = '/')
+    mpeg <- paste(ff.loc, 'ffmpeg.exe', sep = '/')
+  }
 
-# Retrieve FPS
-fps <- system2('c:/ffmpeg/bin/ffprobe.exe', 'p:/obrien/aris/2014-07-16_214123_1_495.mp4 -show_entries stream=r_frame_rate -of default=nokey=1:noprint_wrappers=1 -v warning',
-             stdout=T)
-fps <- as.numeric(strsplit(fps, '/')[[1]][1])
+  # Retrieve number of frames for naming
+  frames <- system2(probe, paste(video, '-show_entries stream=nb_frames',
+                           '-of default=nokey=1:noprint_wrappers=1 -v warning'),
+                    stdout = T)
+  frames <- nchar(frames)
+
+  # Define FFmpeg input string
+  input <-  paste('-i ', video, ' -f image2 ', prefix, '-%', frames,
+                  'd.jpeg -v warning', sep = '')
+
+  # Call system command prompt. If there's a warning message, something is wrong.
+  system2(mpeg, input)
+}
+
+## Example
+# frame_split(video = 'p:/obrien/aris/2014-07-16_214123_1_495.mp4',
+#             ff.loc = 'c:/ffmpeg/bin',
+#             prefix = 'aris')
