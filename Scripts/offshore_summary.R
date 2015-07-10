cc3fc8f7f26e2dc5f57a327570d81fe489bc0fd1
mhpob <mikeob9@gmail.com>
2015-07-10 14:25:06 -0400

    Update for July cruise.

--- Scripts/offshore_summary.R
+++ Scripts/offshore_summary.R
@@ -5,17 +5,63 @@ dets <- filter(dets, grepl('v-|t-|a-', dets$station, ignore.case = T))
 # levels(factor(dets$transmitter))
 
 # Number of unique fish detected, per station
-num_dets <- dets %>% 
+n_dets_all <- dets %>% 
+  group_by(station) %>% 
+  summarize(a = n_distinct(transmitter))
+
+# n_dets_201504 <- dets %>% 
+#   filter(date.local <= lubridate::ymd('20150422', tz = "America/New_York")) %>% 
+#   group_by(station) %>% 
+#   summarize(a = n_distinct(transmitter))
+
+n_dets_201507 <- dets %>% 
+  filter(date.local > lubridate::ymd('20150422', tz = "America/New_York"),
+         date.local <= lubridate::ymd('20150709', tz = "America/New_York")) %>% 
   group_by(station) %>% 
   summarize(a = n_distinct(transmitter))
 
 # Detections in wind energy area.
 species <- left_join(dets, ACTtrans, by = c('transmitter' = 'Tag.ID.Code.Standard'))
 
-num_species <- species %>% group_by(station, Common.Name) %>% 
+n_spec_all <- species %>% group_by(station, Common.Name) %>% 
   distinct(station, transmitter) %>% 
-    summarize(n = n())
+  summarize(n = n())
+
+# n_spec_201504 <- species %>%
+#   filter(date.local <= lubridate::ymd('20150422', tz = "America/New_York")) %>%
+#   group_by(station, Common.Name) %>% 
+#   distinct(station, transmitter) %>% 
+#   summarize(n = n())
+
+n_spec_201507 <- species %>% 
+  filter(date.local > lubridate::ymd('20150422', tz = "America/New_York"),
+         date.local <= lubridate::ymd('20150709', tz = "America/New_York")) %>%
+  group_by(station, Common.Name) %>% 
+  distinct(station, transmitter) %>% 
+  summarize(n = n())
+
+# Species, Institution, PI
+PI_all <- species %>% 
+  group_by(Common.Name, Release.Location, Primary.Researcher,
+           Primary.Tagging.Organization) %>% 
+  distinct(transmitter) %>%
+  summarize(n = n())
+
+# PI_201504 <- species %>%
+#   filter(date.local <= lubridate::ymd('20150422', tz = "America/New_York")) %>%
+#   group_by(Common.Name, Release.Location, Primary.Researcher,
+#            Primary.Tagging.Organization) %>% 
+#   distinct(transmitter) %>%
+#   summarize(n = n())
+
+PI_201507 <- species %>% 
+  filter(date.local > lubridate::ymd('20150422', tz = "America/New_York"),
+         date.local <= lubridate::ymd('20150709', tz = "America/New_York")) %>%
+  group_by(Common.Name, Release.Location, Primary.Researcher,
+           Primary.Tagging.Organization) %>% 
+  distinct(transmitter) %>%
+  summarize(n = n())
 
 # Species in wind energy area.
-WEA_species <- filter(species, station == 'T-2C')
-levels(factor(species$Common.Name))
+WEA_species <- filter(species, station == 'A-5C')
+levels(factor(WEA_species$Common.Name))
