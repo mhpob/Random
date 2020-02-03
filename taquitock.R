713616e1527125ccb067a3e3c9fafbbe8461323e
mhpob <mikeob9@gmail.com>
2020-02-03 18:38:48 -0500

    Better buffer and create STL file.

--- taquitock.R
+++ taquitock.R
@@ -19,7 +19,7 @@ library(dplyr)
 library(stars)
 
 elev_data <- read_stars('USGS_NED_one_meter_x28y416_VA_Sandy_2014_IMG_2015.img', 
-  proxy = T)
+                        proxy = T)
 
 
 # Convert lat/long of property to UTM and buffer point with 137 m (encompasses property)
@@ -68,20 +68,26 @@ raymat <- ray_shade(elev_data,
 elev_data %>%
   sphere_shade(colorintensity = 50) %>%
   add_shadow(raymat, max_darken = 0.5) %>%
-  plot_3d(elev_data, theta = 195, phi = 20, 
+  plot_3d(elev_data, theta = 195, phi = 20, baseshape = 'circle',
           zoom = 0.35, zscale = 0.55, windowsize = c(1000, 900))
 
 render_snapshot(clear = T, instant_capture = F)
 rgl::rgl.close()
 
+elev_data %>%
+  sphere_shade() %>%
+  plot_3d(elev_data, zscale = 0.35, theta = 195,phi = 20)
 
+save_3dprint('taquitock_3d_2.stl', maxwidth = 100)
+
+rgl::readSTL('taquitock_3d.stl', col = 'green')
 
 ## Sun path over Taquitock ----
 # Following http://matthewkling.github.io/media/rayshader/
 library(insol)
 
 sun <- seq(ISOdate(2020, 12, 21, 0, tz = 'America/New_York'),
-          ISOdate(2020, 12, 21, 24, tz = 'America/New_York'), by = "min") %>%
+           ISOdate(2020, 12, 21, 24, tz = 'America/New_York'), by = "min") %>%
   JD() %>%
   sunvector(37.527999, -77.384226, -5) %>%
   sunpos() %>% 
@@ -123,37 +129,147 @@ img %>% image_animate(fps = 10) %>% image_write("animation.gif")
 
 ## Overlay image onto 3D elevation ----
 # This currently doesn't work.
-
+library(stars)
 
 # Write proxy of JPEG2000 image
 pic <- read_stars('c:/users/secor/downloads/m_3707729_se_18_1_20160723_20160928.jp2',
                   proxy = T)
 
+house <- c(-77.384226, 37.527999) %>%
+  # Make coordinate into a point
+  st_point() %>%
+  # Make point into an simple feature (sf object)
+  st_sfc() %>% 
+  # Make coordinate reference system lat/long
+  st_set_crs(4326) %>%
+  # Transform CRS to the proper UTM (CRS of elev_data)
+  st_transform(st_crs(pic)) %>%
+  # Create 137m radius buffer
+  st_buffer(137)
 
-# Convert property buffer to pic CRS and crop pic
-house <- house %>%
-  st_transform(st_crs(pic))
 
 pic <- st_crop(pic, house)
 
 # Convert elev_dat to pic CRS
 elev_data <- st_transform(elev_data, st_crs(pic))
+elev_data <- st_as_stars(elev_data)
 
-pic_full <- adrop(pic)
-pic_full <- pic_full[,,,1]
-pic_full <- st_as_stars(pic_full)
+pic_full <- st_as_stars(pic)
 
 
 
 library(rayshader)
-p <- st_as_raster(pic_full)
+p <- as(pic_full[,,], 'Raster')
 p <- raster_to_matrix(p)
+
+x <- as(x, 'Raster')
+x <- raster_to_matrix(x)
+
 
 pp <- png::readPNG('test.png')
 
-elev_data %>%
+x %>%
   sphere_shade(colorintensity = 50, sunangle = 45) %>%
-  # add_overlay(p) %>%
+  add_overlay(p, alphalayer = 0.5) %>%
   plot_map()
 
 
+
+
+
+## Other data sources ----
+library(stars)
+
+tt <- read_stars('c:/users/secor/downloads/Job505045_va2014_usgs_cmgp_sandy_m4970/Job505045_va2014_usgs_cmgp_sandy_m4970.tif',
+                 proxy = T)
+house <- c(-77.384226, 37.527999) %>%
+  # Make coordinate into a point
+  st_point() %>%
+  # Make point into an simple feature (sf object)
+  st_sfc() %>% 
+  # Make coordinate reference system lat/long
+  st_set_crs(4326) %>%
+  # Transform CRS to the proper UTM (CRS of elev_data)
+  st_transform(st_crs(tt)) %>%
+  # Create 137m radius buffer
+  st_buffer(137)
+
+
+tt <- tt[house]
+
+
+library(rayshader)
+
+
+# Pull elevation data into memory as a raster object, then convert to matrix for
+# rayshader
+
+tt <- tt %>%
+  as('Raster') %>%
+  raster_to_matrix()
+
+
+# Throw some shade
+
+tt %>%
+  sphere_shade(colorintensity = 50, sunangle = 270) %>%
+  plot_map()
+
+raymat <- ray_shade(tt,
+                    sunangle = 73)
+
+tt %>%
+  sphere_shade(colorintensity = 50) %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  plot_3d(tt, theta = 195, phi = 20, baseshape = 'circle',
+          zoom = 0.35, zscale = 0.55, windowsize = c(1000, 900))
+
+
+
+
+
+
+
+## LIDAR ----
+library(lidR)
+
+tt_las <- readLAS('c:/users/secor/downloads/USGS_LPC_VA_Sandy_2014_18STG8956_LAS_2015/USGS_LPC_VA_Sandy_2014_18STG8956_LAS_2015.las')
+
+library(sf)
+house <- c(-77.384226, 37.527999) %>%
+  # Make coordinate into a point
+  st_point() %>%
+  # Make point into an simple feature (sf object)
+  st_sfc() %>% 
+  # Make coordinate reference system lat/long
+  st_set_crs(4326) %>%
+  # Transform CRS to the proper UTM (CRS of elev_data)
+  st_transform(st_crs(tt_las)) %>%
+  # Create 137m radius buffer
+  st_buffer(137)
+
+
+tt_las <- lasclip(tt_las, matrix(st_bbox(house), ncol = 2))
+
+plot(tt_las)
+
+tt_dsm <- grid_canopy(tt_las, res = 0.5,
+                      pitfree(c(0, 2, 5, 10, 15), c(0, 1.5)))
+
+library(rayshader)
+p <- tt_dsm %>% 
+  raster_to_matrix()
+
+
+# Throw some shade
+
+p %>%
+  sphere_shade(colorintensity = 50, sunangle = 270) %>%
+  plot_map()
+
+raymat <- ray_shade(p,
+                    sunangle = 73)
+
+p %>%
+  sphere_shade(colorintensity = 50) %>%
+  plot_3d(p, theta = 195, phi = 20,zscale = 0.5, mouseMode = c('trackball', 'zoom'))
